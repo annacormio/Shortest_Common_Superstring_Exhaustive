4071143f661f939ee3ddeb8f15505ac505647a7d
Anna <annacormio01@gmail.com>
2023-05-09 10:46:25 +0200

    comments done

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def getSubstrings(seq, length=100):  # returns the list of subsequences of the g
 
 def permutations(l): #create a list of lists of all possible permutations of the subsequences entered
     comb=[]
-    for subset in itertools.permutations(l, len(l)):
-        comb.append(list(subset))
+    for p in itertools.permutations(l, len(l)):
+        comb.append(list(p)) #append the permutation to a list in list format
     return comb
 
 
@@ -27,40 +27,43 @@ def overlap_or_merge(s1, s2): #if 2 seq overlap they are joined with the overlap
             pass  # go on looking
         else:  # when subsequence coincide --> overlapping sequence
             over = s2[:i]  # the i-th subsequence is assigned as an overlap
-    if over!='':
+    if over!='': #overlap sequence exist
         l=len(over)
-        seq=s1[:-l]+over+s2[l:]
-    else:
-        seq=s1+s2
+        seq=s1[:-l]+over+s2[l:] #joimed overlapped sequence
+    else: #if no overlap exist
+        seq=s1+s2 #just merge the 2 sequences
     return seq
 
 
 #BUILD SEQ FROM PERMUTATION
 def buildSeq(p): #given a permutation it reconstruct the sequence
     c=p.copy()
-    if len(p)==1:
+    if len(p)==1: #base case: only one seq left in the list --> seq obtained from that permutation joinings
         return p[0]
     else:
-        s=overlap_or_merge(p[0],p[1])
-        c[0]=s
-        c.remove(p[1])
-        return buildSeq(c)
+        s=overlap_or_merge(p[0],p[1]) #overlap or merge the first 2 sequences in the list
+        c[0]=s #the obtaied seq is replaced in first position
+        c.remove(p[1]) #second seq is removed
+        return buildSeq(c) #recursive call
 
 
 def SCS(permutations): #returns the shortest common superstring comparing all joined permutations length
     scs=buildSeq(permutations[0]) #initialize scs
     min_l=len(scs) #initializing the SCS min length
-    for p in permutations:
-        seq=buildSeq(p)
-        if len(seq)<min_l:
-            scs=p
+    for p in permutations: #for each permutaitn of suoerstring
+        seq=buildSeq(p) #assemble the subseq in the permutation by overlap or merge
+        if len(seq)<min_l: #if its len is less than the shortes common superstring length
+            scs=p #it becomes the shortes common superstirng
             min_l=len(p)
     return scs
 
 #MAIN
 if __name__ == '__main__':
-    DNA=getGenome(12)
-    #print(DNA)
+    DNA=input('insert a DNA sequence (if u enter without inserting it I will generate one random)')
+    if DNA=='':
+        DNA=getGenome(12)
+    print(DNA)
+
     subseq=getSubstrings(DNA,6)
     #print(subseq)
     p=permutations(subseq)
